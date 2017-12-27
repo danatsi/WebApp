c87c49201a6d51d18b73e753e76572352fdb0d11
danatsi <dana.tsir@gmail.com>
2017-12-27 17:16:43 +0200

    adding students table

--- pages/Students/students.controller.js
+++ pages/Students/students.controller.js
@@ -1,8 +1,21 @@
 /**
  * Created by Noy Yizchaki on 12/24/2017.
  */
-angular.module('MyApp').controller('StudentsController', function($scope) {
+angular.module('MyApp').controller('StudentsController', function($scope, $http) {
 
     // create a message to display in our view
     $scope.message = 'Look! I am an about page.';
+    // create a message to display in our view
+    $scope.message = 'Look! I am an about page.';
+    $http({
+        method: "GET",
+        url: 'https://randomuser.me/api/?results=50',
+        params: {
+            dataType: "json"
+        }
+    }).then(function (response) {
+        $scope.studentList = response.data.results;
+        console.log(response);
+
+    })
 });
